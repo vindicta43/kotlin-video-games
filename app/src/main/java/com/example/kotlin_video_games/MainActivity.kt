e60b943299e63f6869076318e3c2f9095c57485f
vindicta43 <alperen.erdogan.neu@gmail.com>
2021-03-25 20:16:02 +0300

    basic fragment transactions applied

--- app/src/main/java/com/example/kotlin_video_games/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_video_games/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.kotlin_video_games
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.kotlin_video_games.fragments.FavoriteFragment
+import com.example.kotlin_video_games.fragments.HomeFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val homeFragment = HomeFragment()
+        val favoriteFragment = FavoriteFragment()
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationBar)
+
+        makeFragment(homeFragment)
+
+        bottomNav.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.ic_home -> makeFragment(homeFragment)
+                R.id.ic_favorite -> makeFragment(favoriteFragment)
+            }
+            true
+        }
     }
+
+    private fun makeFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frameLayout, fragment)
+            commit()
+        }
 }
